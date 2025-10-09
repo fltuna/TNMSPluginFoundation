83be23f762ee0415ff9aefbdf131b814ff4e834e
faketuna <faketuna.st@gmail.com>
2025-10-10 03:22:44 +0900

    modify: made only init

--- TnmsDatabaseUtil.Shared/DbConnectionParameters.cs
+++ TnmsDatabaseUtil.Shared/DbConnectionParameters.cs
@@ -4,16 +4,16 @@ namespace TnmsDatabaseUtil.Shared;
 
 public class DbConnectionParameters
 {
-    public TnmsDatabaseProviderType ProviderType { get; set; }
+    public TnmsDatabaseProviderType ProviderType { get; init; }
     
     /// <summary>
     /// When provider type is SQLite, this will be file name (relative to module directory)
     /// </summary>
-    public string? Host { get; set; }
-    public string? Port { get; set; }
-    public string? Database { get; set; }
-    public string? Username { get; set; }
-    public string? Password { get; set; }
+    public string? Host { get; init; }
+    public string? Port { get; init; }
+    public string? Database { get; init; }
+    public string? Username { get; init; }
+    public string? Password { get; init; }
     
     public Dictionary<string, string> AdditionalParameters { get; } = new();
 }
